5847877b2bc92fa84643a603cdb8e898004eb1ad
maycon-santana <maycon.santana@thstec.com.br>
2023-03-05 00:06:00 -0300

    Implementando o controller de AuthUser

---
+++ backend/src/controllers/user/AuthUserController.ts
@@ -0,0 +1,24 @@
+import { Request, Response } from 'express'
+import { AuthUserService } from '../../services/users/AuthUserServices'
+
+class AuthUserController{
+    async handle(req: Request, res: Response){
+        try {
+            const { email, password } = req.body
+
+            const authUserService = new AuthUserService()
+
+            const { token } = await authUserService.execute({
+                email,
+                password
+            })
+
+            return res.json({ token })
+
+        } catch (error) {
+            return res.status(500).json({ error: 'Erro interno do servidor' })
+        }
+    }
+}
+
+export { AuthUserController }
