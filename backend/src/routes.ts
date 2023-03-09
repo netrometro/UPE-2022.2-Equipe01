570901f57234d5bf6f3fa50deb507b08e288edea
maycon-santana <maycon.santana@thstec.com.br>
2023-03-09 11:59:14 -0300

    Atualização de rotas

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -5,7 +5,7 @@ import { AuthUserController } from './controllers/user/AuthUserController'
 
 const router = Router()
 
-router.post('/users', new CreateUserController().handle)
-router.post('/login', new AuthUserController().handle)
+router.post('/api/users', new CreateUserController().handle)
+router.post('/api/login', new AuthUserController().handle)
 
 export { router }
