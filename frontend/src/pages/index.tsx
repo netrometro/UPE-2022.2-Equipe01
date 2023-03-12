dcb1d381d1903a042835ec1d08435bd48cdc3a1b
maycon-santana <maycon.santana@thstec.com.br>
2023-03-11 21:50:51 -0300

    Implementando a função handleLogin

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,3 +1,4 @@
+import { useContext, FormEvent, useState } from 'react'
 import Head from 'next/head'
 import styles from '../../styles/Home.module.scss'
 import Image from 'next/image'
@@ -5,11 +6,40 @@ import Image from 'next/image'
 import { Input } from '../components/ui/Input/index'
 import { Button } from '../components/ui/Button'
 
+import { AuthContext } from '../contexts/AuthContext'
+
 import logoImg from '../../public/logo.svg'
 
 import Link from 'next/link';
 
 export default function Home() {
+  const { signIn } = useContext(AuthContext)
+
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const [loading, setLoading] = useState(false)
+
+  async function handleLogin(event: FormEvent) {
+    event.preventDefault()
+
+    if(email == '' || password === ''){
+      alert('Preencha os Dados')
+      return
+    }
+
+    setLoading(true)
+
+    let data = {
+      email,
+      password
+    }
+
+    await signIn(data)
+
+    setLoading(false)
+  }
+
   return (
     <>
       <Head>
@@ -19,17 +49,23 @@ export default function Home() {
         <Image src={logoImg} alt="natubanho" />
 
        <div className={styles.login}>
-          <form>
+          <form onSubmit={handleLogin}>
              <Input
                 placeholder='Digite seu e-mail'
+                type="text"
+                value={email}
+                onChange={ (e) => setEmail(e.target.value) }
              />
              <Input
                 placeholder='Digite sua senha'
+                type="password"
+                value={password}
+                onChange={ (e) => setPassword(e.target.value) }
              />
 
             <Button
               type="submit"
-              loading={false}
+              loading={loading}
             >
               Acessar
             </Button>
