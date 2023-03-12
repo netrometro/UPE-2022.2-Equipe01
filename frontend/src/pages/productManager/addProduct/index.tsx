438c064830665dd84c6f0329d5f4c6ebc8db6986
Ronald Macario <ronaldmacario13@gmail.com>
2023-03-11 23:12:03 -0300

    Adicionando inputs

--- frontend/src/pages/productManager/addProduct/index.tsx
+++ frontend/src/pages/productManager/addProduct/index.tsx
@@ -7,3 +7,17 @@ function addProduct() {
     <Head>
         <title> Gerenciador de Produtos</title>
     </Head>
+    <div>
+        Adicione seu produto aqui!
+        <div style={{ display: 'flex', flexDirection: 'column', width: 200}}>
+            <input placeholder='Digite o nome do produto' type="text" />
+            <input placeholder='Digite o preço do produto' type="text" />
+            <input placeholder='Digite a quant. a inserir' type="text" />
+            <button> Submeter </button>
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default addProduct
