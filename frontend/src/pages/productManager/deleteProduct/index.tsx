fbf35788e094eb2a0eea3a2bd5dfe46b704e8d4d
Ronald Macario <ronaldmacario13@gmail.com>
2023-03-11 23:13:17 -0300

    Colocando inputs do DELETE

--- frontend/src/pages/productManager/deleteProduct/index.tsx
+++ frontend/src/pages/productManager/deleteProduct/index.tsx
@@ -7,3 +7,17 @@ function deleteProduct() {
     <Head>
         <title> Gerenciador de Produtos</title>
     </Head>
+    <div>
+        Delete seu produto aqui!
+        <div style={{ display: 'flex', flexDirection: 'column', width: 200}}>
+            <select name="nome do produto" id="asdasd"></select>
+            <input placeholder='Digite o preço do produto' type="text" />
+            <input placeholder='Digite a quant. a inserir' type="text" />
+            <button> Submeter </button>
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default deleteProduct
