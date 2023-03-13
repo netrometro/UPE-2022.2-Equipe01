b766548828bd3b2993e27152626c2395b193fb6b
Ronald Macario <ronaldmacario13@gmail.com>
2023-03-12 23:10:53 -0300

    Tentando achar o erro do front em não adicionar

--- backend/src/controllers/user/CreateProductController.ts
+++ backend/src/controllers/user/CreateProductController.ts
@@ -1,14 +1,20 @@
-import { Request, Response } from "express";
+import e, { Request, Response } from "express";
 import { CreateProductService } from "../../services/users/CreateProductService";
 
 export class CreateProductController {
     async handle(req: Request, res: Response) {
-        const {name, price, quantity} = req.body;
+        try {
+            const {name, price, quantity} = req.body;
+    
+            const createProductServices = new CreateProductService();
+    
+            const result = await createProductServices.execute({name, price, quantity});
+    
+            return res.status(201).json(result);
 
-        const createProductServices = new CreateProductService();
-
-        const result = await createProductServices.execute({name, price, quantity});
-
-        return res.status(201).json(result);
+        } catch(err) {
+            console.log(err);
+            res.status(404).json({error:err})
+        }
     }
 }
