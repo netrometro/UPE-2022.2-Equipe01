70176c1d04a114b39231cfedebd92c6b5047ab8c
Rônald Macário <ronaldmacario13@gmail.com>
2023-03-19 13:28:07 -0300

    Tratando exceções de deleteProduct, está tudo funcionando

--- backend/src/services/products/DeleteProductService.ts
+++ backend/src/services/products/DeleteProductService.ts
@@ -1,58 +1,65 @@
-// import { Product } from "@prisma/client";
-// import prismaClient from "../../prisma";
-
-// interface DeleteProductDTO {
-//     id: number;
-// }
-
-// export class DeleteProductService {
-//     async execute({id}): Promise<Product> {
-        
-//         // Verificar se não existe.
-//         // const productAlreadyNotExists = await prismaClient.product.findUnique({
-//         //     where: {
-//         //         id
-//         //     }
-//         // });
-        
-//         var product: Product
-
-//         // if (productAlreadyNotExists) {
-//             // Deletar o produto.
-//             product = await prismaClient.product.delete({
-//                 where: {
-//                     id
-//                 }
-//             })
-//         // } else {
-//         //     // Erro
-//         //     throw new Error("Este produto não existe.")
-//         // }
-
-//         return product
-//     }
-// }
-
-interface ProductDTO {
-    id: number;
-}
-
 import { Product } from "@prisma/client";
 import prismaClient from "../../prisma";
 
+interface DeleteProductDTO {
+    id: number;
+}
+
 export class DeleteProductService {
-    async execute({id}: ProductDTO): Promise<Product> {
-        try {
-            const product = await prismaClient.product.delete({
+    async execute({id}): Promise<Product> {
+        
+        // Verificar se não existe.
+        const productAlreadyNotExists = await prismaClient.product.findUnique({
+            where: {
+                id
+            }
+        });
+        
+        var product: Product
+
+        if (productAlreadyNotExists) {
+            // Deletar o produto.
+            product = await prismaClient.product.delete({
                 where: {
                     id
                 }
-            });
-            return product;
-        } catch(err) {
-            // throw new Error('deu erro aquidddd')
-            console.log(err)
+            })
+        } else {
+            // Erro
+            throw new Error("Este produto não existe.")
         }
-        }
-        
+
+        return product
+    }
 }
+
+
+
+
+
+
+
+
+// interface ProductDTO {
+//     id: number;
+// }
+
+// import { Product } from "@prisma/client";
+// import prismaClient from "../../prisma";
+
+// export class DeleteProductService {
+//     async execute({id}: ProductDTO): Promise<Product> {
+//         try {
+//             const product = await prismaClient.product.delete({
+//                 where: {
+//                     id
+//                 }
+//             });
+//             return product;
+//         } catch(err) {
+//             // throw new Error('deu erro aquidddd')
+//             console.log(err)
+//         }
+//         }
+        
+// }
