a43f66cec59e78e67fbdcba57bc4e6ceb04c5bef
Rônald Macário <ronaldmacario13@gmail.com>
2023-03-19 14:50:41 -0300

    Criei o controlador CreateCartController

---
+++ backend/src/controllers/cart/CreateCartController.ts
@@ -0,0 +1,20 @@
+import e, { Request, Response } from "express";
+import { CreateCartService } from "../../services/cart/CreateCartService";
+
+export class CreateCartController {
+    async handle(req: Request, res: Response) {
+        try {
+            const {productId} = req.body;
+    
+            const createCartServices = new CreateCartService();
+    
+            const result = await createCartServices.execute({productId});
+    
+            return res.status(201).json(result);
+
+        } catch(err) {
+            console.log(err);
+            res.status(404).json({error:err})
+        }
+    }
+}
