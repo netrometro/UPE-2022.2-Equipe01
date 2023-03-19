871f8c9e42c2ae65e4692a3702337b5070acbf06
Rônald Macário <ronaldmacario13@gmail.com>
2023-03-19 18:03:15 -0300

    Corrigindo bugs do banco de dados

---
+++ backend/prisma/migrations/20230319183441_modificacoes_no_carrinho/migration.sql
@@ -0,0 +1,22 @@
+/*
+  Warnings:
+
+  - You are about to drop the `Cart` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropForeignKey
+ALTER TABLE "Cart" DROP CONSTRAINT "Cart_productId_fkey";
+
+-- DropTable
+DROP TABLE "Cart";
+
+-- CreateTable
+CREATE TABLE "cart" (
+    "id" SERIAL NOT NULL,
+    "productId" INTEGER NOT NULL,
+
+    CONSTRAINT "cart_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "cart" ADD CONSTRAINT "cart_productId_fkey" FOREIGN KEY ("productId") REFERENCES "product"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
