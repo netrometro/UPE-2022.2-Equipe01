644987080911ca9d410bff9a4e93f84b282986a1
Rônald Macário <ronaldmacario13@gmail.com>
2023-03-23 18:38:32 -0300

    Melhorei a forma como o carrinho funciona

--- frontend/src/pages/cart/addCart/index.tsx
+++ frontend/src/pages/cart/addCart/index.tsx
@@ -1,4 +1,4 @@
-import { useState, FormEvent, useContext } from 'react'
+import { useState, FormEvent, useContext, useEffect } from 'react'
 
 import Head from 'next/head'
 import React from 'react'
@@ -9,31 +9,25 @@ import { toast } from 'react-toastify'
 // import { ProductContext } from '../../../contexts/Teste'
 import { Button } from '../../../components/ui/Button'
 import { Input } from '../../../components/ui/Input'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+import { type } from 'os'
 
 export default function AddCart() {
 
   const [productId, setProductId] = useState('')
 
-//   const [loading, setLoading] = useState(false)
-  
+  const router = useRouter();
+  const { id } = router.query;
+
   async function handleProductUp(event: FormEvent) {
-     event.preventDefault()
-
-     if(productId === ''){
-      toast.warning('Preencha todos os campos!')
-      return
-     }
-
-    //  setLoading(true)
-
      let data = {
-        productId: Number(productId)
+        productId: Number(id)
      }
 
-    //  await addProduct(data)
      try{
 
-        var response = await api.post('/api/addcart', data)
+        var response = api.post('/api/addcart', data)
         console.log(response);
         toast.success('Produto cadastrado com sucesso!')
 
@@ -41,26 +35,18 @@ export default function AddCart() {
         console.log({err})
      }
 
-
-    //  setLoading(false)
-
   }
-  
+
   return (
     <>
     <Head>
         <title>Criar um carrinho</title>
     </Head>
     <div>
-        Crie seu carrinho aqui!
+        Confime a adição do produto!
+        
         <div style={{ display: 'flex', flexDirection: 'column', width: 200}}>
             <form onSubmit={handleProductUp}>
-            <Input 
-              placeholder='Digite o id do produto'
-              type="text"
-              value={productId}
-              onChange={ (e) => setProductId(e.target.value) }
-            />
             <Button
                 type="submit"
                 loading={false}
@@ -69,7 +55,98 @@ export default function AddCart() {
             </Button>
             </form>
         </div>
+        <Link href="/cart/getCart" legacyBehavior>
+            <a>
+                <button type="button">Ver carrinho</button>
+            </a>
+        </Link>
+        <Link href="/productManager/getProduct" legacyBehavior>
+            <a>
+                <button type="button">Adicionar mais produtos</button>
+            </a>
+        </Link>
     </div>
     </>
   )
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+// export default function AddCart() {
+
+//   const [productId, setProductId] = useState('')
+
+//   const [loading, setLoading] = useState(false)
+// async function handleProductUp() {
+//   async function handleProductUp(event: FormEvent) {
+//      event.preventDefault()
+
+//      if(productId === ''){
+//       toast.warning('Preencha todos os campos!')
+//       return
+//      }
+
+//     //  setLoading(true)
+
+//      let data = {
+//         productId: Number(productId)
+//      }
+
+//     //  await addProduct(data)
+//      try{
+
+//         var response = api.post('/api/addcart', data)
+//         console.log(response);
+//         toast.success('Produto cadastrado com sucesso!')
+
+//       } catch(err) {
+//         console.log({err})
+//      }
+
+
+//     //  setLoading(false)
+
+//   }
+  
+//   return (
+//     <>
+//     <Head>
+//         <title>Criar um carrinho</title>
+//     </Head>
+//     <div>
+//         Crie seu carrinho aqui!
+//         <div style={{ display: 'flex', flexDirection: 'column', width: 200}}>
+//             <form onSubmit={handleProductUp}>
+//             <Input 
+//               placeholder='Digite o id do produto'
+//               type="text"
+//               value={productId}
+//               onChange={ (e) => setProductId(e.target.value) }
+//             />
+//             <Button
+//                 type="submit"
+//                 loading={false}
+//                 >
+//                 Submeter
+//             </Button>
+//             </form>
+//         </div>
+//         <Link href="/cart/getCart" legacyBehavior>
+//             <a>
+//                 <button type="button">Ver carrinho</button>
+//             </a>
+//         </Link>
+//     </div>
+//     </>
+//   )
+// }}
