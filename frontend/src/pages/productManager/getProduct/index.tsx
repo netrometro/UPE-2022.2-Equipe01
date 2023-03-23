33758061598841328fb76ecf94192ceded37ae77
Rônald Macário <ronaldmacario13@gmail.com>
2023-03-23 18:39:37 -0300

    Adicionei alguns botões

--- frontend/src/pages/productManager/getProduct/index.tsx
+++ frontend/src/pages/productManager/getProduct/index.tsx
@@ -1,5 +1,7 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import Link from 'next/link';
+import { Button } from "../../../components/ui/Button";
 
 const GetProducts = () => {
 const [products, setProducts] = useState([]);
@@ -16,16 +18,30 @@ axios
     }, []);
 
 return (
-<div>
-    <h1>Products</h1>
-    {products.map((product, index) => (
-    <div key={index}>
-        <h2>{product.name}</h2>
-        <p>Price: {product.price}</p>
-        <p>Quantity: {product.quantity}</p>
+<>
+    <div>
+        <h1>Products</h1>
+        {products.map((product, index) => (
+        <div key={index}>
+            <h2>{product.name}</h2>
+            <p>ID: {product.id}</p>
+            <p>Price: {product.price}</p>
+            <p>Quantity: {product.quantity}</p>
+
+            <Link href={`/cart/addCart?id=${product.id}`} legacyBehavior>
+                <Button type="button">Adicionar ao carrinho</Button>
+            </Link>
+        </div>
+        ))}
     </div>
-    ))}
-</div>
+    <div>
+        <Link href="/cart/getCart" legacyBehavior>
+            <a>
+                <button type="button">Ver carrinho</button>
+            </a>
+        </Link>
+    </div>
+</>
 );
 };
 
