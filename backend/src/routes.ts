0a5f9c2bb2f84aad3f53b5aedcaf04a5a96c32e2
Rônald Macário <ronaldmacario13@gmail.com>
2023-03-26 23:29:56 -0300

    Adicionando e alterando routes

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -3,10 +3,11 @@ import { Router } from 'express'
 import { CreateUserController } from './controllers/user/CreateUserController'
 import { AuthUserController } from './controllers/user/AuthUserController'
 import { CreateProductController } from './controllers/products/CreateProductController'
-import { GetProductController } from './controllers/products/GetProductController'
+import { GetProductsController } from './controllers/products/GetProductController'
 import { DeleteProductController } from './controllers/products/DeleteProductController'
 import { CreateCartController } from './controllers/cart/CreateCartController'
 import { GetCartController } from './controllers/cart/GetCartController'
+import { GetProductIdController } from './controllers/products/GetProductIdController'
 
 const router = Router()
 
@@ -16,7 +17,8 @@ router.post('/api/login', new AuthUserController().handle)
 
 // Products
 router.post('/api/product', new CreateProductController().handle)
-router.get("/api/getproducts", new GetProductController().handle)
+router.get("/api/getproducts", new GetProductsController().handle)
+router.get("/api/getproductid", new GetProductIdController().handle)
 router.delete("/api/deleteproduct", new DeleteProductController().handle)
 
 // Cart
