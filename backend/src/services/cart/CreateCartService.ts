0589de8154e359e335c3d63a64f1f6ff4c389349
Rônald Macário <ronaldmacario13@gmail.com>
2023-04-02 17:42:15 -0300

    Alterando o CREATE do Cart

--- backend/src/services/cart/CreateCartService.ts
+++ backend/src/services/cart/CreateCartService.ts
@@ -3,14 +3,18 @@ import prismaClient from "../../prisma";
 
 interface CartDTO {
     productId: number;
+    price: number;
+    quantity: number;
 }
 
 export class CreateCartService {
-    async execute({productId}: CartDTO): Promise<Cart> {
+    async execute({productId, price, quantity}: CartDTO): Promise<Cart> {
             //Criar o carrinho.
             const cart = prismaClient.cart.create({
                 data: {
-                    productId
+                    productId,
+                    price,
+                    quantity
                 }
             });
     
