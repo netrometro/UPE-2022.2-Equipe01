2ac4996898ac6fd4754097ba12e3d5e26001612f
Ronald Macario <ronaldmacario13@gmail.com>
2023-04-10 00:15:27 -0300

    Removendo comentários

--- frontend/src/pages/productManager/getProducts/index.tsx
+++ frontend/src/pages/productManager/getProducts/index.tsx
@@ -1,58 +1,3 @@
-// import axios from "axios";
-// import { useEffect, useState } from "react";
-// import Link from 'next/link' 
-// import { Button } from "../../../components/ui/Button";
-
-// const GetProducts = () => {
-// const [products, setProducts] = useState([]);
-
-// useEffect(() => {
-// axios
-//     .get('http://localhost:3333/api/getproducts')
-//     .then((response) => {
-//     setProducts(response.data);
-//     })
-//     .catch((error) => {
-//     console.log(error);
-//     });
-//     }, []);
-
-// return (
-// <>
-//     <div>
-//         <h1>Products</h1>
-//         {products.map((product, index) => (
-//         <div key={index}>
-//             <h2>{product.name}</h2>
-//             <p>ID: {product.id}</p>
-//             <p>Price: {product.price}</p>
-//             <p>Quantity: {product.quantity}</p>
-
-//             <Link href={`/productManager/getProductId/${product.id}`} legacyBehavior>
-//                 <Button type="button">Mais informações</Button>
-//             </Link>
-
-//             <Link href={`/cart/addCart?id=${product.id}`} legacyBehavior>
-//                 <Button type="button">Adicionar ao carrinho</Button>
-//             </Link>
-//         </div>
-//         ))}
-//     </div>
-//     <div>
-//         <Link href="/cart/getCart" legacyBehavior>
-//             <a>
-//                 <button type="button">Ver carrinho</button>
-//             </a>
-//         </Link>
-//     </div>
-// </>
-// );
-// };
-
-// export default GetProducts;
-
-
-
 import axios from "axios";
 import { FormEvent, useEffect, useState } from "react";
 import Link from 'next/link' 
@@ -60,7 +5,6 @@ import { Button } from "../../../components/ui/Button";
 import { setupAPIClient } from "../../../services/api";
 import { toast } from "react-toastify";
 import { api } from "../../../services/apiClient";
-import { randomBytes } from "crypto";
 
 const GetProducts = () => {
 const [products, setProducts] = useState([]);
