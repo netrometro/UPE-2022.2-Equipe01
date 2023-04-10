4550a8559beda948541654fb435713e686ca0026
Ronald Macario <ronaldmacario13@gmail.com>
2023-04-10 00:14:40 -0300

    Adicionando pagina de cada produto

--- frontend/src/pages/productManager/getProductId/index.tsx
+++ frontend/src/pages/productManager/getProductId/index.tsx
@@ -1,9 +1,31 @@
+import { useRouter } from 'next/router';
 import React from 'react'
+import { api } from '../../../services/apiClient';
+import axios from 'axios';
 
 const ProductId = () => {
+    const router = useRouter();
+    const { id } = router.query;
+
+    async function getProductById() {
+        let data = {
+            id: Number(2)
+        }
+
+        try{
+            const response = await api.get('/api/getproductid', {data});
+            console.log(response);
+        } catch(err) {
+            console.log(err)
+        }
+
+        
+    }
+
     return (
         <>
             <h3>Listar item específico</h3>
+            <button onClick={getProductById}>teste</button>
         </>
     )
 }
