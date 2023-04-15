7929197082035c4c7428ab131453bd1b33d05664
Ronald Macario <ronaldmacario13@gmail.com>
2023-04-15 12:08:38 -0300

    Adicionando contador no item específico

--- frontend/src/pages/productManager/getProductId/index.tsx
+++ frontend/src/pages/productManager/getProductId/index.tsx
@@ -2,6 +2,10 @@ import { useRouter } from 'next/router';
 import React, { useEffect, useState } from 'react'
 import { api } from '../../../services/apiClient';
 import axios from 'axios';
+import { toast } from 'react-toastify';
+import { setupAPIClient } from '../../../services/api';
+import { Button } from '../../../components/ui/Button';
+import Header from '../../../components/Header';
 
 interface iProduct {
     id: number;
@@ -14,7 +18,8 @@ interface iProduct {
 const ProductId = () => {
 
     const [product, setProduct] = useState<iProduct> ();
-    const [test, setTest] = useState("Testando");
+    
+    const [count, setCount] = useState(0);
 
     const router = useRouter();
     const { id } = router.query;
@@ -23,6 +28,7 @@ const ProductId = () => {
         id: Number(id)
     }
 
+    
     useEffect(() => {
         const fetchProduct = async () => {
             try{
@@ -35,13 +41,68 @@ const ProductId = () => {
                 console.log(error);
             }
         };
-
+        
         fetchProduct();
-
+        
     }, []);
 
+    function increaseCount() {
+        setCount(count + 1);
 
+    }
 
+    function decreaseCount() {
+        if (count > 0) {
+            setCount(count - 1);
+    };
+    }
+
+    async function handleUpdate() {
+    
+        const id = product.id
+
+        const nome = product.name
+
+        const price =  product.price
+
+        const quantity = product.quantity - count
+    
+        const apiClient = setupAPIClient()
+    
+        const produto = {
+            id: Number(id),
+            name: String(nome),
+            price: Number(price),
+            quantity: Number(quantity)
+        }
+    
+        let data = {
+            productId: Number(id),
+            quantity: Number(count),
+            price: Number(produto.price*count)
+            }
+        
+    
+        if (produto.quantity < 0) {
+            toast.error("Quantidade inválida!")
+        } else{
+            await apiClient.put('/api/updateproduct', produto)
+    
+            try{
+    
+                var response = api.post('/api/addcart', data)
+                console.log(response);
+                toast.success('Adicionado ao carrinho!')
+        
+            } catch(err) {
+                console.log({err})
+            }
+        
+        }
+        
+        
+    }
+        
     return (
         <div>
             <Header/>
@@ -49,6 +110,16 @@ const ProductId = () => {
             <p>ID: {product?.id}</p>
             <p>Price: {product?.price}</p>
             <p>Quantity: {product?.quantity}</p>
+
+            <Button id="btn" type="button" onClick={handleUpdate}>Adicionar ao carrinho</Button>
+
+            <button type="button" onClick={decreaseCount}>
+            -
+            </button>
+            <span>{count}</span>
+            <button type="button" onClick={increaseCount}>
+            +
+            </button>
         </div>
     )
 }
